70aad69bdb12faccb0325014c2784d71a21e5b5a
sleepy <nemorzgm.05@gmail.com>
2024-12-06 13:23:28 +0800

    test

--- admintest.py
+++ admintest.py
@@ -15,9 +15,9 @@ def connect_to_database():
     """Connect to the MySQL database."""
     return mysql.connector.connect(
         host="localhost",
-        user="root",  # Replace with your MySQL username
-        password="",  # Replace with your MySQL password
-        database="apv"  # Replace with your database name
+        user="root",  
+        password="",  
+        database="apv"  
     )
 
 
